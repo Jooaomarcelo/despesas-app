17e9529dca853f201415d4c13501fd655fb554e7
João Marcelo <JoaoMdSB@hotmail.com>
2024-12-11 15:15:46 -0300

    Added and styled transaction card

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'models/Transaction.dart';
 
-void main() => runApp(const ExpensesApp());
+void main() => initializeDateFormatting('pt_BR', null)
+    .then((_) => runApp(const ExpensesApp()));
 
 class ExpensesApp extends StatelessWidget {
   const ExpensesApp({super.key});
@@ -33,27 +36,72 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Despesas Pessoais'),
-          centerTitle: true,
-          backgroundColor: Colors.blueAccent[400],
-          foregroundColor: Colors.white,
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            SizedBox(
-              child: Card(
-                color: Colors.blueAccent[400],
-                elevation: 5,
-                child: const Text('Gráfico'),
-              ),
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent[400],
+        foregroundColor: Colors.white,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          SizedBox(
+            child: Card(
+              color: Colors.blueAccent[400],
+              elevation: 5,
+              child: const Text('Gráfico'),
             ),
-            const Card(
-              child: Text('Lista de Transações'),
-            )
-          ],
-        ));
+          ),
+          Column(
+            children: _transactions
+                .map((tr) => Card(
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 10),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: const Color.fromRGBO(11, 120, 216, 1),
+                                width: 2,
+                              ),
+                            ),
+                            padding: const EdgeInsets.all(10),
+                            child: Text(
+                              'R\$${tr.value.toStringAsFixed(2)}',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Color.fromRGBO(11, 120, 216, 1),
+                              ),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                tr.title,
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                DateFormat('d MMM y', 'pt_BR').format(tr.date),
+                                style: TextStyle(
+                                  color: Colors.grey[700],
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ))
+                .toList(),
+          ),
+        ],
+      ),
+    );
   }
 }
