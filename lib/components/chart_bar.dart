a50290b92737950e66328e2d3b4811c3be7bcbba
Jooaomarcelo <joaomdsb@hotmail.com>
2024-12-12 14:35:40 -0300

    Added chart bar

---
+++ lib/components/chart_bar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String label;
+  final double percentage;
+  final double value;
+
+  const ChartBar({
+    required this.label,
+    required this.percentage,
+    required this.value,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('R\$${value.toStringAsFixed(2)}'),
+        const SizedBox(height: 5),
+        Container(
+          height: 60,
+          width: 10,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(height: 5),
+        Text(label),
+      ],
+    );
+  }
+}
