bf03e04b5ab941ec69416aadb53f2df5765f92fb
Jooaomarcelo <joaomdsb@hotmail.com>
2024-12-13 13:28:27 -0300

    Added swipe for editing to transaction items

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -5,8 +5,14 @@ import '../models/transaction.dart';
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
   final Function(String) onDelete;
+  final Function(BuildContext, {Transaction tr}) openTransactionFormModal;
 
-  const TransactionList(this.transactions, this.onDelete, {super.key});
+  const TransactionList({
+    required this.transactions,
+    required this.onDelete,
+    required this.openTransactionFormModal,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -36,33 +42,51 @@ class TransactionList extends StatelessWidget {
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               final tr = transactions[index];
-              return Card(
-                elevation: 3,
-                margin: const EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 5,
+              return Dismissible(
+                key: Key(tr.id),
+                direction: DismissDirection.startToEnd,
+                confirmDismiss: (direction) async {
+                  await openTransactionFormModal(context, tr: tr);
+                  return false;
+                },
+                background: Container(
+                  color: Theme.of(context).colorScheme.primary,
+                  alignment: Alignment.centerLeft,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: const Icon(
+                    Icons.edit,
+                    color: Colors.white,
+                    size: 40,
+                  ),
                 ),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30,
-                    child: Padding(
-                      padding: const EdgeInsets.all(10),
-                      child: FittedBox(
-                        child: Text('R\$${tr.value}'),
+                child: Card(
+                  elevation: 3,
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 5,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: FittedBox(
+                          child: Text('R\$${tr.value}'),
+                        ),
                       ),
                     ),
-                  ),
-                  title: Text(
-                    tr.title,
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  subtitle: Text(DateFormat.yMMMd(
-                          Localizations.localeOf(context).toString())
-                      .format(tr.date)),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.delete),
-                    color: Theme.of(context).colorScheme.error,
-                    onPressed: () => onDelete(tr.id),
+                    title: Text(
+                      tr.title,
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                    subtitle: Text(DateFormat.yMMMd(
+                            Localizations.localeOf(context).toString())
+                        .format(tr.date)),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      color: Theme.of(context).colorScheme.error,
+                      onPressed: () => onDelete(tr.id),
+                    ),
                   ),
                 ),
               );
